da39536613fa85d731ca4851a9b7f7f40c593129
JulianCasafus <julian.casafus@celuweb.com>
2025-11-14 16:47:36 -0500

    commit

--- src/main/java/com/spotify/interactions/Condicional.java
+++ src/main/java/com/spotify/interactions/Condicional.java
@@ -4,11 +4,17 @@ import com.spotify.ui.LoginUI;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Interaction;
 import net.serenitybdd.screenplay.actions.Click;
+import net.serenitybdd.screenplay.waits.WaitUntil;
+
+import static net.serenitybdd.screenplay.matchers.WebElementStateMatchers.isVisible;
 
 public class Condicional implements Interaction {
 
     @Override
     public <T extends Actor> void performAs(T actor) {
+        actor.attemptsTo(
+                WaitUntil.the(LoginUI.BTN_INICIAR_CON_CONTRA, isVisible()).forNoMoreThan(20).seconds()
+        );
         if (LoginUI.BTN_INICIAR_CON_CONTRA.resolveFor(actor).isVisible()) {
             actor.attemptsTo(
                     Click.on(LoginUI.BTN_INICIAR_CON_CONTRA)
